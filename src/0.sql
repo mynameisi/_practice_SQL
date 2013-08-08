2284dbed7072fe86b9289d114962875959f33918
mynameisi <mynamestillisi@gmail.com>
2013-08-09 00:39:18 +0800

    增加了0.sql的comment

--- src/0.sql
+++ src/0.sql
@@ -1,3 +1,5 @@
 --这不是一个练习
---运行项目测试，如果这个测试不通过，表示Java项目没有成功搭建
+--运行项目测试文件：右键点击项目-->选择"Run as"-->选择"JUnit Test"
+--不要管Console窗口中出现的异常提示，看看JUnit窗口中testSQL0是不是绿色的
+--如果不是，表示Java项目没有成功搭建
 select name from customer;
