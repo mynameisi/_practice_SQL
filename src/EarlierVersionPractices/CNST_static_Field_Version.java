9b0aaab46eb86bae46f1ba034f531d1f807d6c90
mynameisi <mynamestillisi@gmail.com>
2013-08-11 13:44:46 +0800

    把不用的类注释掉了

--- src/EarlierVersionPractices/CNST_static_Field_Version.java
+++ src/EarlierVersionPractices/CNST_static_Field_Version.java
@@ -1,76 +1,76 @@
-package EarlierVersionPractices;
-
-import helper.FileIO;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Properties;
-
-public class CNST_static_Field_Version {
-
-	private static Properties prop = null;
-
-	//数据库相关字段
-	public static String DRIVER = null;
-	public static String DB_URL = null;
-	public static String USER = null;
-	public static String PASS = null;
-	
-	//用来控制Msg类-->从而控制在运行程序时显示何种信息
-	public static Boolean DEBUG_MSG = null;
-	public static Boolean USER_MSG = null;
-	
-	public static Charset CHAR_ENCODING = null;
-	
-	//导出标准输入输出流的地址
-	public static String STDOUT=null;
-	public static String STDERR=null;
-	
-	//为了Debug方便，控制JUnit时间间隔
-	public static int INTERVAL_JUNIT=0;
-	//关闭数据库的线程等待多长时间关闭数据库连接
-	public static int INTERVAL_DBSHUTDOWN=0;
-
-	static {
-		if (prop == null) {
-			prop = new Properties();
-			FileInputStream fis;
-			try {
-				File ppxml=FileIO.findSiblingResource(CNST_static_Field_Version.class, "properties.xml");
-				fis = new FileInputStream(ppxml);
-				prop.loadFromXML(fis);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		if(Boolean.parseBoolean(prop.getProperty("REDIRECT"))){
-			STDOUT = prop.getProperty("STDOUT");
-			STDERR = prop.getProperty("STDERR");
-			FileIO.redirect(STDOUT,STDERR);
-		}
-		
-		INTERVAL_JUNIT = Integer.parseInt(prop.getProperty("INTERVAL_JUNIT"));
-		INTERVAL_DBSHUTDOWN= Integer.parseInt(prop.getProperty("INTERVAL_DBSHUTDOWN"));
-		
-		DEBUG_MSG = DEBUG_MSG != null ? DEBUG_MSG : Boolean.parseBoolean(prop.getProperty("DEBUG_MSG"));
-		USER_MSG = USER_MSG != null ? USER_MSG : Boolean.parseBoolean(prop.getProperty("USER_MSG"));
-		switch (prop.getProperty("CHAR_ENCODING")) {
-		case "UTF-8":
-			CHAR_ENCODING = StandardCharsets.UTF_8;
-		}
-	
-		String dbNow=prop.getProperty("DB_NOW");
-		DRIVER = (DRIVER != null ? DRIVER : prop.getProperty(dbNow+"_DRIVER"));
-		DB_URL = (DB_URL != null ? DB_URL : prop.getProperty(dbNow+"_URL"));
-		USER = (USER != null ? USER : prop.getProperty(dbNow+"_USER"));
-		PASS = (PASS != null ? PASS : prop.getProperty(dbNow+"_PASS"));
-
-	}
-
-	public static String getSQL(String sqlFile) {
-		return prop.getProperty(sqlFile);
-	}
-
-}
+//package EarlierVersionPractices;
+//
+//import helper.FileIO;
+//
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.nio.charset.Charset;
+//import java.nio.charset.StandardCharsets;
+//import java.util.Properties;
+//
+//public class CNST_static_Field_Version {
+//
+//	private static Properties prop = null;
+//
+//	//数据库相关字段
+//	public static String DRIVER = null;
+//	public static String DB_URL = null;
+//	public static String USER = null;
+//	public static String PASS = null;
+//	
+//	//用来控制Msg类-->从而控制在运行程序时显示何种信息
+//	public static Boolean DEBUG_MSG = null;
+//	public static Boolean USER_MSG = null;
+//	
+//	public static Charset CHAR_ENCODING = null;
+//	
+//	//导出标准输入输出流的地址
+//	public static String STDOUT=null;
+//	public static String STDERR=null;
+//	
+//	//为了Debug方便，控制JUnit时间间隔
+//	public static int INTERVAL_JUNIT=0;
+//	//关闭数据库的线程等待多长时间关闭数据库连接
+//	public static int INTERVAL_DBSHUTDOWN=0;
+//
+//	static {
+//		if (prop == null) {
+//			prop = new Properties();
+//			FileInputStream fis;
+//			try {
+//				File ppxml=FileIO.findSiblingResource(CNST_static_Field_Version.class, "properties.xml");
+//				fis = new FileInputStream(ppxml);
+//				prop.loadFromXML(fis);
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		if(Boolean.parseBoolean(prop.getProperty("REDIRECT"))){
+//			STDOUT = prop.getProperty("STDOUT");
+//			STDERR = prop.getProperty("STDERR");
+//			FileIO.redirect(STDOUT,STDERR);
+//		}
+//		
+//		INTERVAL_JUNIT = Integer.parseInt(prop.getProperty("INTERVAL_JUNIT"));
+//		INTERVAL_DBSHUTDOWN= Integer.parseInt(prop.getProperty("INTERVAL_DBSHUTDOWN"));
+//		
+//		DEBUG_MSG = DEBUG_MSG != null ? DEBUG_MSG : Boolean.parseBoolean(prop.getProperty("DEBUG_MSG"));
+//		USER_MSG = USER_MSG != null ? USER_MSG : Boolean.parseBoolean(prop.getProperty("USER_MSG"));
+//		switch (prop.getProperty("CHAR_ENCODING")) {
+//		case "UTF-8":
+//			CHAR_ENCODING = StandardCharsets.UTF_8;
+//		}
+//	
+//		String dbNow=prop.getProperty("DB_NOW");
+//		DRIVER = (DRIVER != null ? DRIVER : prop.getProperty(dbNow+"_DRIVER"));
+//		DB_URL = (DB_URL != null ? DB_URL : prop.getProperty(dbNow+"_URL"));
+//		USER = (USER != null ? USER : prop.getProperty(dbNow+"_USER"));
+//		PASS = (PASS != null ? PASS : prop.getProperty(dbNow+"_PASS"));
+//
+//	}
+//
+//	public static String getSQL(String sqlFile) {
+//		return prop.getProperty(sqlFile);
+//	}
+//
+//}
--- src/EarlierVersionPractices/HSQLDB.java
+++ src/EarlierVersionPractices/HSQLDB.java
@@ -1,210 +1,210 @@
-package EarlierVersionPractices;
-
-import java.io.File;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Scanner;
-
-import org.hsqldb.server.Server;
-
-public class HSQLDB {
-
-	Server server = null;
-	Connection conn = null; //our connnection to the db - presist for life of program
-
-	// we dont want this garbage collected until we are done
-	public HSQLDB(String db_file_name_prefix) throws Exception { // note more general exception
-
-		// Load the HSQL Database Engine JDBC driver
-		// hsqldb.jar should be in the class path or made part of the current jar
-		Class.forName("org.hsqldb.jdbcDriver");
-
-		// connect to the database.   This will load the db files and start the
-		// database if it is not alread running.
-		// db_file_name_prefix is used to open or create files that hold the state
-		// of the db.
-		// It can contain directory names relative to the
-		// current working directory
-		conn = DriverManager.getConnection("jdbc:hsqldb:" + db_file_name_prefix + ";shutdown=true", // filenames
-				"sa", // username
-				""); // password
-	}
-
-	public HSQLDB(String db_file_name_prefix, String serverAddress, int portNumber) throws Exception {
-		server = new Server();
-		server.setAddress("localhost");
-		server.setDatabaseName(0, "testdb");
-		server.setDatabasePath(0, db_file_name_prefix);
-		server.setPort(1234);
-		server.setTrace(true);
-		server.setLogWriter(new PrintWriter(System.out));
-		server.start();
-		try {
-			Class.forName("org.hsqldb.jdbc.JDBCDriver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace(System.out);
-		}
-		try {
-			String connectionURL = "jdbc:hsqldb:hsql://" + serverAddress + ":" + portNumber + "/testdb";
-			System.out.println(connectionURL);
-			conn = DriverManager.getConnection(connectionURL, "SA", "");
-		} catch (SQLException e) {
-			e.printStackTrace(System.out);
-		}
-	}
-
-	public void shutdown() throws SQLException {
-
-		Statement st = conn.createStatement();
-
-		// db writes out to files and performs clean shuts down
-		// otherwise there will be an unclean shutdown
-		// when program ends
-		st.execute("SHUTDOWN");
-		if (conn != null) {
-			conn.close(); // if there are no other open connection
-		}
-		if (server != null) {
-			server.shutdown();
-		}
-	}
-
-	//use for SQL command SELECT
-	public synchronized void query(String expression) throws SQLException {
-
-		Statement st = null;
-		ResultSet rs = null;
-
-		st = conn.createStatement(); // statement objects can be reused with
-
-		// repeated calls to execute but we
-		// choose to make a new one each time
-		rs = st.executeQuery(expression); // run the query
-
-		// do something with the result set.
-		dump(rs);
-		st.close(); // NOTE!! if you close a statement the associated ResultSet is
-
-		// closed too
-		// so you should copy the contents to some other object.
-		// the result set is invalidated also  if you recycle an Statement
-		// and try to execute some other query before the result set has been
-		// completely examined.
-	}
-
-	//use for SQL commands CREATE, DROP, INSERT and UPDATE
-	public synchronized void update(String expression) throws SQLException {
-		Statement st = null;
-		st = conn.createStatement(); // statements
-		int i = st.executeUpdate(expression); // run the query
-		if (i == -1) {
-			System.out.println("db error : " + expression);
-		}
-		st.close();
-	} // void update()
-
-	public synchronized void batchUpdate(File f) throws Exception {
-		Statement st = null;
-		st = conn.createStatement(); // statements
-		Scanner sc = null;
-		sc = new Scanner(f);
-		StringBuilder sql = new StringBuilder();
-		while (sc.hasNextLine()) {
-			String sqlLine = sc.nextLine().trim();
-			if (sqlLine.startsWith("--")) {
-				continue;
-			}
-			sql.append(sqlLine + ' ');
-			if (!sqlLine.contains(";")) {
-				continue;
-			}
-			String finalSQL = sql.toString();
-			System.out.println(finalSQL);
-			st.executeUpdate(finalSQL);
-			sql = new StringBuilder();
-		}
-		System.out.println("creation done");
-		st.clearBatch();
-		sc.close();
-	}
-
-	public static void dump(ResultSet rs) throws SQLException {
-
-		// the order of the rows in a cursor
-		// are implementation dependent unless you use the SQL ORDER statement
-		ResultSetMetaData meta = rs.getMetaData();
-		int colmax = meta.getColumnCount();
-		int i;
-		Object o = null;
-
-		// the result set is a cursor into the data.  You can only
-		// point to one row at a time
-		// assume we are pointing to BEFORE the first row
-		// rs.next() points to next row and returns true
-		// or false if there is no next row, which breaks the loop
-		for (; rs.next();) {
-			for (i = 0; i < colmax; ++i) {
-				o = rs.getObject(i + 1); // Is SQL the first column is indexed
-
-				// with 1 not 0
-				System.out.print(o.toString() + " ");
-			}
-
-			System.out.println(" ");
-		}
-	} //void dump( ResultSet rs )
-
-	public static void main(String[] args) {
-
-		HSQLDB db = null;
-
-		try {
-			db = new HSQLDB("db_file");
-		} catch (Exception ex1) {
-			ex1.printStackTrace(); // could not start db
-
-			return; // bye bye
-		}
-
-		try {
-
-			//make an empty table
-			//
-			// by declaring the id column IDENTITY, the db will automatically
-			// generate unique values for new rows- useful for row keys
-			db.update("CREATE TABLE sample_table ( id INTEGER IDENTITY, str_col VARCHAR(256), num_col INTEGER)");
-		} catch (SQLException ex2) {
-
-			//ignore
-			//ex2.printStackTrace();  // second time we run program
-			//  should throw execption since table
-			// already there
-			//
-			// this will have no effect on the db
-		}
-
-		try {
-
-			// add some rows - will create duplicates if run more then once
-			// the id column is automatically generated
-			db.update("INSERT INTO sample_table(str_col,num_col) VALUES('Ford', 100)");
-			db.update("INSERT INTO sample_table(str_col,num_col) VALUES('Toyota', 200)");
-			db.update("INSERT INTO sample_table(str_col,num_col) VALUES('Honda', 300)");
-			db.update("INSERT INTO sample_table(str_col,num_col) VALUES('GM', 400)");
-
-			// do a query
-			db.query("SELECT * FROM sample_table WHERE num_col < 250");
-
-			// at end of program
-			db.shutdown();
-		} catch (SQLException ex3) {
-			ex3.printStackTrace();
-		}
-	} // main()
-
-} // class Testdb
+//package EarlierVersionPractices;
+//
+//import java.io.File;
+//import java.io.PrintWriter;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.ResultSetMetaData;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//import java.util.Scanner;
+//
+//import org.hsqldb.server.Server;
+//
+//public class HSQLDB {
+//
+//	Server server = null;
+//	Connection conn = null; //our connnection to the db - presist for life of program
+//
+//	// we dont want this garbage collected until we are done
+//	public HSQLDB(String db_file_name_prefix) throws Exception { // note more general exception
+//
+//		// Load the HSQL Database Engine JDBC driver
+//		// hsqldb.jar should be in the class path or made part of the current jar
+//		Class.forName("org.hsqldb.jdbcDriver");
+//
+//		// connect to the database.   This will load the db files and start the
+//		// database if it is not alread running.
+//		// db_file_name_prefix is used to open or create files that hold the state
+//		// of the db.
+//		// It can contain directory names relative to the
+//		// current working directory
+//		conn = DriverManager.getConnection("jdbc:hsqldb:" + db_file_name_prefix + ";shutdown=true", // filenames
+//				"sa", // username
+//				""); // password
+//	}
+//
+//	public HSQLDB(String db_file_name_prefix, String serverAddress, int portNumber) throws Exception {
+//		server = new Server();
+//		server.setAddress("localhost");
+//		server.setDatabaseName(0, "testdb");
+//		server.setDatabasePath(0, db_file_name_prefix);
+//		server.setPort(1234);
+//		server.setTrace(true);
+//		server.setLogWriter(new PrintWriter(System.out));
+//		server.start();
+//		try {
+//			Class.forName("org.hsqldb.jdbc.JDBCDriver");
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace(System.out);
+//		}
+//		try {
+//			String connectionURL = "jdbc:hsqldb:hsql://" + serverAddress + ":" + portNumber + "/testdb";
+//			System.out.println(connectionURL);
+//			conn = DriverManager.getConnection(connectionURL, "SA", "");
+//		} catch (SQLException e) {
+//			e.printStackTrace(System.out);
+//		}
+//	}
+//
+//	public void shutdown() throws SQLException {
+//
+//		Statement st = conn.createStatement();
+//
+//		// db writes out to files and performs clean shuts down
+//		// otherwise there will be an unclean shutdown
+//		// when program ends
+//		st.execute("SHUTDOWN");
+//		if (conn != null) {
+//			conn.close(); // if there are no other open connection
+//		}
+//		if (server != null) {
+//			server.shutdown();
+//		}
+//	}
+//
+//	//use for SQL command SELECT
+//	public synchronized void query(String expression) throws SQLException {
+//
+//		Statement st = null;
+//		ResultSet rs = null;
+//
+//		st = conn.createStatement(); // statement objects can be reused with
+//
+//		// repeated calls to execute but we
+//		// choose to make a new one each time
+//		rs = st.executeQuery(expression); // run the query
+//
+//		// do something with the result set.
+//		dump(rs);
+//		st.close(); // NOTE!! if you close a statement the associated ResultSet is
+//
+//		// closed too
+//		// so you should copy the contents to some other object.
+//		// the result set is invalidated also  if you recycle an Statement
+//		// and try to execute some other query before the result set has been
+//		// completely examined.
+//	}
+//
+//	//use for SQL commands CREATE, DROP, INSERT and UPDATE
+//	public synchronized void update(String expression) throws SQLException {
+//		Statement st = null;
+//		st = conn.createStatement(); // statements
+//		int i = st.executeUpdate(expression); // run the query
+//		if (i == -1) {
+//			System.out.println("db error : " + expression);
+//		}
+//		st.close();
+//	} // void update()
+//
+//	public synchronized void batchUpdate(File f) throws Exception {
+//		Statement st = null;
+//		st = conn.createStatement(); // statements
+//		Scanner sc = null;
+//		sc = new Scanner(f);
+//		StringBuilder sql = new StringBuilder();
+//		while (sc.hasNextLine()) {
+//			String sqlLine = sc.nextLine().trim();
+//			if (sqlLine.startsWith("--")) {
+//				continue;
+//			}
+//			sql.append(sqlLine + ' ');
+//			if (!sqlLine.contains(";")) {
+//				continue;
+//			}
+//			String finalSQL = sql.toString();
+//			System.out.println(finalSQL);
+//			st.executeUpdate(finalSQL);
+//			sql = new StringBuilder();
+//		}
+//		System.out.println("creation done");
+//		st.clearBatch();
+//		sc.close();
+//	}
+//
+//	public static void dump(ResultSet rs) throws SQLException {
+//
+//		// the order of the rows in a cursor
+//		// are implementation dependent unless you use the SQL ORDER statement
+//		ResultSetMetaData meta = rs.getMetaData();
+//		int colmax = meta.getColumnCount();
+//		int i;
+//		Object o = null;
+//
+//		// the result set is a cursor into the data.  You can only
+//		// point to one row at a time
+//		// assume we are pointing to BEFORE the first row
+//		// rs.next() points to next row and returns true
+//		// or false if there is no next row, which breaks the loop
+//		for (; rs.next();) {
+//			for (i = 0; i < colmax; ++i) {
+//				o = rs.getObject(i + 1); // Is SQL the first column is indexed
+//
+//				// with 1 not 0
+//				System.out.print(o.toString() + " ");
+//			}
+//
+//			System.out.println(" ");
+//		}
+//	} //void dump( ResultSet rs )
+//
+//	public static void main(String[] args) {
+//
+//		HSQLDB db = null;
+//
+//		try {
+//			db = new HSQLDB("db_file");
+//		} catch (Exception ex1) {
+//			ex1.printStackTrace(); // could not start db
+//
+//			return; // bye bye
+//		}
+//
+//		try {
+//
+//			//make an empty table
+//			//
+//			// by declaring the id column IDENTITY, the db will automatically
+//			// generate unique values for new rows- useful for row keys
+//			db.update("CREATE TABLE sample_table ( id INTEGER IDENTITY, str_col VARCHAR(256), num_col INTEGER)");
+//		} catch (SQLException ex2) {
+//
+//			//ignore
+//			//ex2.printStackTrace();  // second time we run program
+//			//  should throw execption since table
+//			// already there
+//			//
+//			// this will have no effect on the db
+//		}
+//
+//		try {
+//
+//			// add some rows - will create duplicates if run more then once
+//			// the id column is automatically generated
+//			db.update("INSERT INTO sample_table(str_col,num_col) VALUES('Ford', 100)");
+//			db.update("INSERT INTO sample_table(str_col,num_col) VALUES('Toyota', 200)");
+//			db.update("INSERT INTO sample_table(str_col,num_col) VALUES('Honda', 300)");
+//			db.update("INSERT INTO sample_table(str_col,num_col) VALUES('GM', 400)");
+//
+//			// do a query
+//			db.query("SELECT * FROM sample_table WHERE num_col < 250");
+//
+//			// at end of program
+//			db.shutdown();
+//		} catch (SQLException ex3) {
+//			ex3.printStackTrace();
+//		}
+//	} // main()
+//
+//} // class Testdb
